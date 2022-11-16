4c4b17d1d8cb314aef7015de065d894719dca2d9
Darya <113056128+Darya2006@users.noreply.github.com>
2022-11-17 00:11:24 +0400

    new

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$('.reviews-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1
+})
+
+sliderSecondBlock()
+function sliderSecondBlock() {
+    // $('.reviews .evriewsBlock .info-block .item').css('opacity', 0.3);
+    // $('.reviews .reviewsBlock .info-block .item.slick-active').eq(1).css('opacity', 1);
+    //$(function().{
+    $('.item1').addClass('prozr');
+    $('.item1.slick-active').eq(1).addClass('opacity');
+    //});
+}
+
